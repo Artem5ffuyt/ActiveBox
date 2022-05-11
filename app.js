96d2c7e95da736503eff8ff2c64e5d519f8fe0bb
Artem Bachynskyi <97476036+Artem5ffuyt@users.noreply.github.com>
2022-05-11 19:39:27 +0300

    test

--- app.js
+++ app.js
@@ -4,13 +4,20 @@ let intro = $("#intro");
 let introH = intro.innerHeight();
 let scrollPos = $(window).scrollTop();
 
-$(window).on("scroll",function(){
+$(window).on("scroll load resize",function(){
+let introH = intro.innerHeight();
 scrollPos = $(this).scrollTop();
+
+if (scrollPos > introH) {
+    header.addClass("fixed");
+} else {
+    header.removeClass("fixed");
+}
 
 console.log(scrollPos);
 });
 
 
 
-header.addClass("fixet");
+header.addClass("fixed");
 });
